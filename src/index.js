1efb47d2aca4e7e528477ce739fa9f580a6d7ec7
unknown <aarguedas@fwdcostarica.com>
2024-08-08 21:38:18 -0600

    cambio2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const botonReiniciar = document.getElementById("botonReiniciar")
 const turnoX = document.getElementById("turnoX")
 const turnoO = document.getElementById("turnoO")
 
+variableGane = "0"
+
+turnoX.innerText = ""
+turnoO.innerText = ""
+
 
 ///////////////recorrido de la lista -divs- y evento al seleccionar casilla////////////////////
                
@@ -42,14 +47,14 @@ for (let index = 0; index < lista.length; index++) {
         let nuevaPosicion = nuevaLista[numeroAleatorio]
 
     
-///////////////////////////////agregar O en el juego///////////////////////////////////////////
+///////////////////////////////agregar O en el juego//////////////////////////////////////////
 
-            
-
-                            lista[nuevaPosicion].innerHTML = "O"; 
-                            lista[nuevaPosicion].style.pointerEvents = "none"  
-                            lista[nuevaPosicion].style.color = "blue" 
-                            lista[nuevaPosicion].style.backgroundColor = "black"
+                            if (nuevaLista.length > 0) {
+                                lista[nuevaPosicion].innerHTML = "O"; 
+                                lista[nuevaPosicion].style.pointerEvents = "none"  
+                                lista[nuevaPosicion].style.color = "blue" 
+                                lista[nuevaPosicion].style.backgroundColor = "black"    
+                            }                           
  
         console.log(lista);
         console.log(nuevaLista);
@@ -63,17 +68,16 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////validacion ganar X filas/////////////////////////////////////////////
 
                             if (divs[0].textContent === "X" && divs[1].textContent === "X" && divs[2].textContent === "X") {
-                                let variableGane = "ganar"
-                                            turnoX.innerText = "Gano1 - X"
+                                 variableGane = "1"
+                                            turnoX.innerText = "Gano1 - X"                       
 
                                    for (let index = 0; index < divs.length; index++) 
-                                                divs[index].style.pointerEvents = "none"
-                                                console.log("debe reiniciar");      
+                                                divs[index].style.pointerEvents = "none"   
                                    }
                                     
 
                             if (divs[3].textContent === "X" && divs[4].textContent === "X" && divs[5].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                             turnoX.innerText = "Gano2 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -82,7 +86,7 @@ for (let index = 0; index < lista.length; index++) {
                                             
                                 
                             if (divs[6].textContent === "X" && divs[7].textContent === "X" && divs[8].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                             turnoX.innerText ="Gano3 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -94,7 +98,7 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////validacion ganar X columnas/////////////////////////////////////////////
 
                             if (divs[0].textContent === "X" && divs[3].textContent === "X" && divs[6].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                 turnoX.innerText = "Gano4 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -102,7 +106,7 @@ for (let index = 0; index < lista.length; index++) {
                                             }
 
                             if (divs[1].textContent === "X" && divs[4].textContent === "X" && divs[7].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                 turnoX.innerText = "Gano5 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -110,7 +114,7 @@ for (let index = 0; index < lista.length; index++) {
                                             }
                                         
                             if (divs[2].textContent === "X" && divs[5].textContent === "X" && divs[8].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                 turnoX.innerText = "Gano6 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -120,7 +124,7 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////validacion ganar X diagonales/////////////////////////////////////////////
 
                             if (divs[0].textContent === "X" && divs[4].textContent === "X" && divs[8].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                 turnoX.innerText = "Gano7 - X";
 
                                     for (let index = 0; index < divs.length; index++) 
@@ -128,7 +132,7 @@ for (let index = 0; index < lista.length; index++) {
                                         }
 
                             if (divs[2].textContent === "X" && divs[4].textContent === "X" && divs[6].textContent === "X") {
-                                let variableGane = "ganar"
+                                variableGane = "1"
                                 turnoX.innerText = "Gano8 - X";
                                            
                                     for (let index = 0; index < divs.length; index++) 
@@ -140,24 +144,28 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
 ///////////////////////////////validacion ganar O filas/////////////////////////////////////////////
 
                             if (divs[0].textContent === "O" && divs[1].textContent === "O" && divs[2].textContent === "O") {
-                                turnoO.innerHTML = "Gano1 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano1 - O";
                                            
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"                               
                                         }
 
                             if (divs[3].textContent === "O" && divs[4].textContent === "O" && divs[5].textContent === "O") {
-                            turnoO.innerHTML = "Gano2 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano2 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"
                                         }
                             
                             if (divs[6].textContent === "O" && divs[7].textContent === "O" && divs[8].textContent === "O") {
-                            turnoO.innerHTML = "Gano3 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano3 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"
@@ -166,21 +174,24 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////validacion ganar O columnas/////////////////////////////////////////////
 
                             if (divs[0].textContent === "O" && divs[3].textContent === "O" && divs[6].textContent === "O") {
-                                turnoO.innerHTML = "Gano4 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano4 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"
                                         }
 
                             if (divs[1].textContent === "O" && divs[4].textContent === "O" && divs[7].textContent === "O") {
-                                turnoO.innerHTML = "Gano5 - 0";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano5 - 0";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"                                                                    
                                         }
                                         
                             if (divs[2].textContent === "O" && divs[5].textContent === "O" && divs[8].textContent === "O") {
-                                turnoO.innerHTML = "Gano6 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano6 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"                                                                                  
@@ -189,14 +200,16 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////validacion ganar O diagonales/////////////////////////////////////////////
 
                             if (divs[0].textContent === "O" && divs[4].textContent === "O" && divs[8].textContent === "O") {
-                                turnoO.innerHTML = "Gano7 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano7 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"                                                                                
                                         }
 
                             if (divs[2].textContent === "O" && divs[4].textContent === "O" && divs[6].textContent === "O") {
-                                turnoO.innerHTML = "Gano8 - O";
+                                variableGane = "1"
+                                turnoO.innerText = "Gano8 - O";
                                           
                                     for (let index = 0; index < divs.length; index++) 
                                                 divs[index].style.pointerEvents = "none"                                                                                                    
@@ -205,33 +218,20 @@ for (let index = 0; index < lista.length; index++) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                 
-
 ///////////////////////////////////validacion empates/////////////////////////////////////////////////   
 
-                if (lista === "X", "X", "X", "X", "X" || "O", "O", "O", "O", "O") {
-                    if (variableGane = "ganar") {
-                            console.log("GANE");  
-                    }else{
-                        console.log("hiciste un empate");
-                        
-                    }
-                }
+                        if (nuevaLista.length === 0 || (turnoX.innerText != "" || turnoO.innerText != "")) {
+                            if (variableGane === "0") {
+                                console.log("empate");  
+                            }else{
+                            console.log("ganaste");
 
+                            }
+                        }
 
-
-
-
-
-
-
-
-
-///////////////////////////////////validacion perder/////////////////////////////////////////////////  
-
-
+     
     })
 }
-
 
 
 
